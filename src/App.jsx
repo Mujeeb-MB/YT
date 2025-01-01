b66b4e6e04f2b6e5531282f4a3afe6603a6f3907
mujeeb <md.mujeeb5577@gmail.com>
2025-01-01 09:04:25 +0530

    added new tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const openInNewTab = () => {
+    const url =
+      "https://www.youtube.com/embed/JKccS9k56_I?si=Bx-Kstup48bnj52I&autoplay=1&fs=1";
+    window.open(url, "_blank");
+  };
 
   return (
     <>
@@ -14,11 +15,11 @@ function App() {
         height="315"
         src="https://www.youtube.com/embed/JKccS9k56_I?si=Bx-Kstup48bnj52I"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        referrerPolicy="strict-origin-when-cross-origin"
       ></iframe>
+      <button onClick={openInNewTab}>Open Fullscreen in New Tab</button>
     </>
   );
 }
